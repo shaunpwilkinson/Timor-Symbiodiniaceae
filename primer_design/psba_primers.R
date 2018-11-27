b85ed4691a3edd221cc339db48ef9b75a0852548
Shaun Wilkinson <shaunpwilkinson@gmail.com>
2018-11-27 19:46:18 +0900

    Added psbA primer design script and alignments

--- primer_design/psba_primers.R
+++ primer_design/psba_primers.R
@@ -1,85 +1,70 @@
+#####################################################################################
+############### PRIMER DESIGN SCRIPT FOR UNIVERSAL PSBANCR PRIMERS ##################
+#####################################################################################
+
 library(insect)
 
-setwd("~/Dropbox/East_Timor/Rutherford_Project/Part3_treecongruence/Josh_Paper1/R/PSBA/")
+## download reference sequences from GenBank
 rdb <- searchGB(paste0("Symbiodiniaceae[ORGN]+AND+PSBA[GENE]+NOT+predicted[TITL]",
                        "+NOT+unverified[TITL]+NOT+environmental[ORGN]+AND+100:5000[SLEN]"))
+taxids <- as.integer(sub(".+\\|", "", names(rdb)))
+tdb <- taxonomy() ## downloaded Nov 2018
+tdb <- prune_taxonomy(tdb, taxids)
 saveRDS(rdb, file = "rdb.rds")
-## reference sequence - coding sequence postion looked up manually
+saveRDS(tdb, file = "primer_design/tdb.rds")
 
-rdb <- readRDS("rdb.rds")
-tdb <- readRDS("~/Dropbox/R/insect/taxonomy/tdb_full.rds") # Nov 2018 version
-
-psbA_1.0 <- "CWGTAGATATTGATGGWATAAGAGA"
-psbA_3.0 <- "TTGAAAGCCATTGTYCTTACTCC"
-
+## read in databases
+rdb <- readRDS("primer_design/rdb.rds") # reference database
+tdb <- readRDS("primer_design/rdb.rds") # taxonomy map
 SYMPSBANCRF <- "AATCGTGCTGATCTAGGWATGG"
 SYMPSBANCRR <- "GAGACGATTTGTTGTGGATAG"
 
-test <- virtualPCR(rdb, up = SYMPSBANCRF, down = SYMPSBANCRR, trimprimers = FALSE, cores = 4)
-##11 seqs
-forw_ali <- virtualPCR(rdb, up = SYMPSBANCRF, trimprimers = FALSE, cores = 4) #44
-rev_ali <- virtualPCR(rdb, up = rc(SYMPSBANCRR), trimprimers = FALSE, cores = 4) #77
+## trim LHS of primer bind region
+forw_seqs <- virtualPCR(rdb, up = SYMPSBANCRF, trimprimers = FALSE, cores = 6) #44
+rev_seqs <- virtualPCR(rdb, up = rc(SYMPSBANCRR), trimprimers = FALSE, cores = 6) #77
 
+## only keep sequences for which both primer bind regions are available
 keeps <- names(forw_ali) %in% names(rev_ali) #19
-forw_ali <- forw_ali[keeps]
-keeps <- names(rev_ali) %in% names(forw_ali) #19
-rev_ali <- rev_ali[keeps]
+forw_ali <- forw_seqs[keeps]
+keeps <- names(rev_ali) %in% names(forw_ali)
+rev_ali <- rev_seqs[keeps]
 rev_ali <- rev_ali[names(forw_ali)]
 
+## trim RHS
 forw_ali <- shave(forw_ali, char2dna(SYMPSBANCRF))
 rev_ali <- shave(rev_ali, char2dna(rc(SYMPSBANCRR)))
 
+## remove duplicates
 concats <- paste0(sub(".+\\|", "", names(forw_ali)), dna2char(forw_ali), dna2char(rev_ali))
 discards <- duplicated(concats) #10
 forw_ali <- forw_ali[!discards]
 rev_ali <- rev_ali[!discards]
 
+## build alignments
 forw_ali <- aphid::align(forw_ali)
 rev_ali <- aphid::align(rev_ali)
 
+## find lineage metadata
 taxids <- as.integer(sub(".+\\|", "", rownames(forw_ali)))
-get_lineage(taxids, tdb)
+lineages <- get_lineage(taxids, tdb)
+unname(sapply(lineages, tail, 1))
 sub("\\|.+", "", rownames(forw_ali))
 
-tmp1 <- ape::as.character.DNAbin(forw_ali)
-tmp2 <- ape::as.character.DNAbin(rev_ali)
+## print alignments
+matrix(apply(forw_ali, 1, dna2char), ncol = 1)
+matrix(apply(rev_ali, 1, dna2char), ncol = 1)
 
+## find positions in reference sequence
+HG515017_CDS <- structure(rdb["HG515017|431157"][[1]][1:1029], class = "DNAbin")
+match(1, aphid::Viterbi(HG515017_CDS, char2dna(SYMPSBANCRF), type = "semiglobal")$path) #958
+match(1, aphid::Viterbi(HG515017_CDS, char2dna(rc(SYMPSBANCRR)), type = "semiglobal")$path) #58
 
+## add missing clades D, E, F manually if possible
+ftaxids <- as.integer(sub(".+\\|", "", names(forw_seqs)))
+flins <- get_lineage(ftaxids, tdb)
+unname(sapply(flins, tail, 1))
+rtaxids <- as.integer(sub(".+\\|", "", names(rev_seqs)))
+rlins <- get_lineage(rtaxids, tdb)
+unname(sapply(rlins, tail, 1))
 
-
-## virtual PCR to catch sequences that include Pochon14 primer bind region
-z <- virtualPCR(rdb, up = psbA_1.0, down = psbA_3.0, trimprimers = TRUE, cores = 4)
-
-# 26 sequences retained
-## derive profile hidden Markov model
-model <- aphid::derivePHMM(z, cores = 4) ## 670 modules
-## virtualFISH to include those without primer bind regions (i.e. including Pochon's sequence)
-saveRDS(model, file = "model.rds")
-
-## check for high scoring sequences among those initially rejected
-rejects <- !(names(rdb) %in% names(z))
-rejects <- rdb[rejects]
-seconds <- virtualFISH(rejects, probe = model, minscore = 50,
-                       minamplen = 500L, maxamplen = 1000L, cores = 4L)
-## 35 retained
-z <- c(z, seconds)
-taxids <- as.integer(sub(".+\\|", "", names(z)))
-
-lineages <- get_lineage(taxids, tdb)
-vapply(lineages, tail, "", 1, USE.NAMES = FALSE)
-
-## our primers are outside range
-# virtualPCR(z, up = SYMPSBANCRF, trimprimers = TRUE)
-# virtualPCR(z, up = rc(SYMPSBANCRR), trimprimers = TRUE)
-
-
-virtualPCR(symtrim, up = SYMPSBANCRF, trimprimers = TRUE)
-virtualPCR(symtrim, up = rc(SYMPSBANCRR), trimprimers = TRUE)
-aphid::Viterbi(symtrim[1], z[1], type = "semiglobal")$path
-aphid::Viterbi(symtrim[1], char2dna(SYMPSBANCRF), type = "semiglobal")$path
-aphid::Viterbi(symtrim[1], char2dna(rc(SYMPSBANCRR)), type = "semiglobal")$path
-
-
-
-HG515017_CDS <- structure(sym["HG515017|431157"][[1]][1:1029], class = "DNAbin")
-symtrim <- virtualFISH(sym, probe = HG515017_CDS, cores = 4) # 7seqs
+#####################################################################################
